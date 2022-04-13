a719cad33462f372821bd78854fdec8eff41288f
punikson <nomercy314159@gmail.com>
2022-04-13 19:38:51 +0300

    1st commit

--- src/com/punikson/webapp/storage/ArrayStorage.java
+++ src/com/punikson/webapp/storage/ArrayStorage.java
@@ -1,32 +1,37 @@
+package com.punikson.webapp.storage;
+
+import com.punikson.webapp.model.Resume;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++)
             storage[i] = null;
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[size] = r;
         size++;
     }
 
-    Resume get(String uuid) {
-        for (Resume resume : storage)
-            if (uuid.equals(resume.uuid)) {
-                return resume;
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++)
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
             }
+
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++)
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
@@ -36,14 +41,14 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+   public Resume[] getAll() {
         Resume[] result = new Resume[size];
         for (int i = 0; i < size; i++)
             result[i] = storage[i];
         return result;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
